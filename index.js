90c5a234fe170eb4c1d48ab4c8c1159d6a8ec261
md-abir-alahe <mdabiralahe@gmail.com>
2023-05-08 02:12:12 +0600

    create client side post and insert single user data

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 const uri = "mongodb+srv://mdabiralahe:97Zt0a347XlXt6xw@cluster0.rjdbxkw.mongodb.net/?retryWrites=true&w=majority";
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -27,12 +28,26 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("userDB");
+        const userCollection = database.collection("users");
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('new user', user);
+
+            const result = await userCollection.insertOne(user);
+
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.log);
